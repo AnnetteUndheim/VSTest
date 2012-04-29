807c7d8d017ff3792ae319f998520520ef05ba8c
Annette Undheim <annette.undheim@webstep.no>
2012-04-29 20:44:22 +0200

    La til "Hello World"

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 namespace ConsoleApplication1 {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("Hello World");
         }
     }
 }
